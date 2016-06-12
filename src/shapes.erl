55bb6fd01765653f0f9d5f156a1389ee0fcf1e4f
Oscar Rieken <oscar.rieken@adp.com>
2016-06-11 20:06:05 -0400

    added a few more exampls to the area function :chart_with_upward_trend:

--- src/shapes.erl
+++ src/shapes.erl
@@ -1,9 +1,17 @@
 -module(shapes).
 -author("orieken").
+-import(math, [sqrt/1]).
 -export([area/1]).
 
 
 area({square, Side}) ->
   Side * Side;
 area({circle, Radius}) ->
-  3.1416 * Radius * Radius.
+  3.1416 * Radius * Radius;
+area({rectangle, Length, Width}) ->
+  Length * Width;
+area({triangle, A, B, C}) ->
+  Sides = (A + B + C) / 2,
+  math:sqrt(Sides * (Sides - A) * (Sides - B) * (Sides - C));
+area(_Other) ->
+  {error, invalid_opject}.
--- test/shapes_tests.erl
+++ test/shapes_tests.erl
@@ -1,6 +1,6 @@
 -module(shapes_tests).
 -author("orieken").
-
+-import(erlang, [display/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 area_can_calculate_a_square_test() ->
@@ -12,3 +12,19 @@ area_can_calculate_a_circle_test() ->
   Radius = 4,
   Area = shapes:area({circle, Radius}),
   ?assertEqual(Area, 3.1416 * Radius * Radius).
+
+%% length times width
+area_can_calculate_a_rectangle_test() ->
+  Length = 12,
+  Width = 13,
+  Area = shapes:area({rectangle, Length, Width}),
+  ?assertEqual(Area, Length * Width).
+
+area_can_calculate_a_triangle_test() ->
+  A = 5,
+  B = 7,
+  C = 6,
+  Sides = (A + B + C) / 2,
+  Total = math:sqrt(Sides*(Sides-A)*(Sides-B)*(Sides-C)),
+  Area = shapes:area({triangle, A, B, C }),
+  ?assertEqual(Area, Total).
