d5deee839cc8404b0da93c146c7cfb92ae7e7581
Oscar Rieken <oscar.rieken@adp.com>
2016-06-13 00:21:32 -0400

    :vertical_traffic_light: added examples for average

--- test/average_tests.erl
+++ test/average_tests.erl
@@ -25,3 +25,16 @@ average_of_three_test() ->
 average_of_lots_of_numbers_test() ->
   Expected_Average = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10) / 10,
   ?assertEqual(average:average(lists:seq(1, 10)), Expected_Average).
+
+sum_of_zero_test() ->
+  ?assertEqual(average:sum([0]), 0).
+
+sum_of_one_and_two_test() ->
+  Expected_Sum = 1 + 2,
+  ?assertEqual(average:sum([1,2]), Expected_Sum).
+
+sum_of_10_and_20_test() ->
+  ?assertEqual(average:sum([10,20]), 30).
+
+sum_of_10_and_negative_20_test() ->
+  ?assertEqual(average:sum([10,-20]), -10).
